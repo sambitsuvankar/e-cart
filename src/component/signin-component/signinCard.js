1df63b544e8be05c2a865880cd488af45cbcf95e
sambitsuvankr <sambitsuvankarsahoo@gmail.com>
2022-02-05 23:17:40 +0530

    5th Commit

--- src/component/signin-component/signinCard.js
+++ src/component/signin-component/signinCard.js
@@ -5,9 +5,9 @@ import { SignInStart, signOutStart } from "../../redux/user/user.actions";
 const SignInCard = ({signInStart, signOutStart}) => {
     
     return(
-        <div className="bg-white w-80 h-64 rounded-md">
+        <div className="bg-white w-80 h-64 p-2 rounded-md">
             <p className="my-7 font-sans font-medium">Sign in through Google to Access the Cart!</p>
-            <button className="bg-blue-500 my-10 text-white w-1/2 h-1/5 rounded-md focus:bg-indigo-600 hover:bg-blue-700" onClick={signInStart}>Sign In With Google</button>
+            <button className="bg-blue-500 my-10 p-2 text-white w-1/2 h-1/5 rounded-md focus:bg-indigo-600 hover:bg-blue-700" onClick={signInStart}>Sign In With Google</button>
         </div>
     )
 }
